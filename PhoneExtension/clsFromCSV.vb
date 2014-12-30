f91b5b9a4831a36f25f4b64dd82d088f2db8c2ba
sarindy <sarindyo@yahoo.com>
2014-12-30 08:46:51 +0700

    Add

---
+++ PhoneExtension/clsFromCSV.vb
@@ -0,0 +1,48 @@
+﻿Public Class clsFromCSV
+    Private PhoneList As DataTable
+    Public ReadOnly Property GetPhoneList() As DataTable
+        Get
+            Return PhoneList
+        End Get
+    End Property
+
+    Public Sub New()
+        Dim myDataTable As New DataTable
+
+        myDataTable.Columns.Add("Name", System.Type.GetType("System.String"))
+        myDataTable.Columns.Add("Number", System.Type.GetType("System.String"))
+        Dim myRow As DataRow
+
+        
+        Try
+            Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(My.Application.Info.DirectoryPath & "\phonelist.csv")
+                MyReader.TextFieldType = FileIO.FieldType.Delimited
+                MyReader.SetDelimiters(",")
+                Dim currentRow As String()
+                While Not MyReader.EndOfData
+                    Try
+                        currentRow = MyReader.ReadFields()
+                        myRow = myDataTable.NewRow
+                        myRow("Name") = currentRow.GetValue(0)
+                        myRow("Number") = currentRow.GetValue(1)
+
+                        myDataTable.Rows.Add(myRow)
+                       
+                    Catch ex As Microsoft.VisualBasic.
+                                FileIO.MalformedLineException
+                        MsgBox("Line " & ex.Message &
+                        "is not valid and will be skipped.")
+                    End Try
+                End While
+                PhoneList = myDataTable
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            PhoneList = Nothing
+        End Try
+
+
+
+
+    End Sub
+End Class
---
+++ PhoneExtension/frmMain.vb
@@ -0,0 +1,78 @@
+﻿Imports System.Xml
+Public Class frmMain
+    Public objDataTable As New DataTable
+    Public objCSV As New clsFromCSV
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim name As String = InputBox("Please Enter Name to Search... ", "Extension Enquiry", "Mantol")
+
+            If name = "" Then
+                Exit Sub
+            End If
+
+            objDataTable = objCSV.GetPhoneList
+ 
+            Dim result() As DataRow = objDataTable.Select("name like '%" & name & "%'")
+            name = ""
+            ' Loop and display.
+            For Each row As DataRow In result
+                name = name & "Name= " & row(0) & "   Phone No. " & row(1) & vbCrLf
+            Next
+            MsgBox(name)
+
+        Catch ex As ArgumentException
+            MsgBox(ex.Message)
+            'conStr = "N/A"
+        End Try
+    End Sub
+
+    Private Sub frmMain_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        Button1.Focus()
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'objDataTable = callXML()
+
+    End Sub
+    Private Function callXML() As DataTable
+        Try
+
+            Dim m_xmld As XmlDocument
+            Dim m_nodelist As XmlNodeList
+            Dim m_node As XmlNode
+
+            Dim myDataTable As New DataTable
+
+            myDataTable.Columns.Add("Name", System.Type.GetType("System.String"))
+            myDataTable.Columns.Add("Number", System.Type.GetType("System.String"))
+            Dim myRow As DataRow
+            'Create the XML Document
+            m_xmld = New XmlDocument()
+            'Load the Xml file
+            m_xmld.Load(My.Application.Info.DirectoryPath & "\phone_list.xml")
+            'Get the list of name nodes 
+            m_nodelist = m_xmld.SelectNodes("/phone_list/contact")
+            'Loop through the nodes
+            For Each m_node In m_nodelist
+                'Get the firstName Element Value
+                'conStr = m_node.ChildNodes.Item(0).InnerText
+                myRow = myDataTable.NewRow
+                myRow("Name") = m_node.ChildNodes.Item(0).InnerText
+                myRow("Number") = m_node.ChildNodes.Item(1).InnerText
+
+                myDataTable.Rows.Add(myRow)
+
+
+
+            Next
+
+            Return myDataTable
+
+
+        Catch ex As ArgumentException
+            MsgBox(ex.Message)
+            Return Nothing
+        End Try
+    End Function
+End Class
